b5e2e218ac82f165b1ab0a37ec2fae7b75d0476d
Koray Gocmen <gocmen.koray@gmail.com>
2015-11-20 13:15:10 -0500

    Update Resistor.h

--- Resistor.h
+++ Resistor.h
@@ -1,1 +1,35 @@
+#ifndef RESISTOR_H
+#define RESISTOR_H
+#include <string>
+#include <iostream>
+using namespace std;
+class Resistor {
+    
+private:
+	double resistance; 
+	string name; 
+	int endpointNodeIDs[2]; 
+	int rIndex;
+        Resistor *next;
+    
+public:
+	Resistor();
+	Resistor(string name_,double resistance_,int endpoints_[2]);
+	~Resistor();
+        Resistor *getNext() const;
+        void setNext(Resistor *n);
+        bool tailInsert(Resistor* &head,Resistor* &ptr);     
+	void updateResistor(int rIndex_,string name_,double resistance_,int endpoints_[2]);
+	string getName() const; 
+	double getResistance() const;
+	void setResistance (double resistance_);
+	void print ();
+	void printSingleRes();
+        bool findResistor(string name);
+        bool deleteRes(Resistor* &head, string rName);
+        int getEndPoint1();
+        int getEndPoint2();
+    
+};
 
+#endif /* RESISTOR_H */
